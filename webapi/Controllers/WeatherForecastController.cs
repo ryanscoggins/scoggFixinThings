2525afd5b2f94e5e1e61031fd1bbf0321300f69e
Ryan Scoggins <john.scoggins@dodiis.mil>
2023-12-11 19:53:56 -0600

    return posts

--- webapi/Controllers/WeatherForecastController.cs
+++ webapi/Controllers/WeatherForecastController.cs
@@ -22,16 +22,10 @@ public class WeatherForecastController : ControllerBase
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    public List<Post> Get()
     {
-        var test = new List<Post>();
-        test = _context.Posts.ToList();
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        var posts = new List<Post>();
+        posts = _context.Posts.ToList();
+        return posts;
     }
 }
