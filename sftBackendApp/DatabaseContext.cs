14aab566ab5460a879fbe9e5574a694d69690b61
Ryan Scoggins <john.scoggins@dodiis.mil>
2024-01-04 20:41:43 -0600

    attempt at creating backend app connection string to azure db

---
+++ sftBackendApp/DatabaseContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace sftBackendApp
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+        }
+
+    }
+}
