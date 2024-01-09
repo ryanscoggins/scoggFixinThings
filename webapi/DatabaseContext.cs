2282b367dab3b28e193e846337faeb8c49da6d3d
Ryan Scoggins <john.scoggins@dodiis.mil>
2024-01-09 11:17:40 -0600

    api set up

---
+++ webapi/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using webapi.Models;
+
+namespace webapi
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+        }
+
+    }
+}
