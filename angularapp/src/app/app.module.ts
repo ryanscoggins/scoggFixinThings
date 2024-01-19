8c6eb567339c29d305efba27bce9c5feea95fcec
Ryan Scoggins <john.scoggins@dodiis.mil>
2024-01-19 15:04:02 -0600

    header buttons

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -5,13 +5,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 
+//PrimeNG modules
+import { ButtonModule } from 'primeng/button'
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule, HttpClientModule, ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
