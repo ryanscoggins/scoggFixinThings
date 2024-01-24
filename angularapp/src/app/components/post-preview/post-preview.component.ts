b1f832b047b6dcb8985419d4ecf4d30f8d4ee38c
Ryan Scoggins <john.scoggins@dodiis.mil>
2024-01-23 21:06:16 -0600

    add functionality to read more button

--- angularapp/src/app/components/post-preview/post-preview.component.ts
+++ angularapp/src/app/components/post-preview/post-preview.component.ts
@@ -1,10 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { Post } from 'src/interfaces/post.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-post-preview',
   templateUrl: './post-preview.component.html',
 })
 export class PostPreviewComponent {
+  constructor(
+    private router: Router
+  ) {}
+
   @Input() posts!: Post[]
+
+  openPost(postId: string) {
+    this.router.navigate([`post/${postId}`])
+  }
 }
