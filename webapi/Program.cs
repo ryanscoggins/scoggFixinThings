0ed42a130088ee9bcb4151c5e1d45ae0ab1997a8
Ryan Scoggins <john.scoggins@dodiis.mil>
2024-02-05 10:12:29 -0600

    add retry to sql builder, add a no posts template

--- webapi/Program.cs
+++ webapi/Program.cs
@@ -10,7 +10,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("sftDb")));
+builder.Services.AddDbContext<DatabaseContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("sftDb"), builder =>
+    {
+        builder.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null);
+    }
+    ));
 
 builder.Services.AddCors(opt =>
 {
