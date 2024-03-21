c4a58ba4cc7ff3ce0e856f8ee481c95437731994
Ryan Scoggins <john.scoggins@dodiis.mil>
2024-03-20 20:11:42 -0500

    create heartbeat http request

---
+++ webapi/Controllers/HeartBeatController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using webapi.Models;
+
+namespace webapi.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class HeartBeatController : ControllerBase
+    {
+        private readonly ILogger<HeartBeatController> _logger;
+        private readonly DatabaseContext _context;
+
+        public HeartBeatController(ILogger<HeartBeatController> logger, DatabaseContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult CheckPulse()
+        {
+            return Ok(true);
+        }
+    }
+}
