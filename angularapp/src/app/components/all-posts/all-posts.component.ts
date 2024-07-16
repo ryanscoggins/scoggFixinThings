5b6fce00a97d7770db7efe09c5e6a0106865ae53
Ryan Scoggins <john.scoggins@dodiis.mil>
2024-07-16 15:22:26 -0500

    work on all posts in one page

--- angularapp/src/app/components/all-posts/all-posts.component.ts
+++ angularapp/src/app/components/all-posts/all-posts.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
-
+import { PostService } from 'src/services/post.service';
+import { Post } from 'src/interfaces/post.interface';
 @Component({
   selector: 'app-all-posts',
   templateUrl: './all-posts.component.html'
 })
 export class AllPostsComponent {
+  constructor(
+    private postService: PostService
+  ) {};
+
+  allPosts: Post[] = [];
+  loading: boolean = false;
+
+  ngOnInit(): void {
+    this.loading = true;
+    this.postService.getPosts().subscribe((data) => {
+      this.allPosts = data;
+      this.loading = false;
+    })
+   }
 
 }
